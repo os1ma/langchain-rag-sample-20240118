ba78c47adac797d09f7ab6687dd3d4ebf78ae3aa
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2024-01-18 17:05:29 +0900

    Add type hint

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import Runnable, RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 load_dotenv()
@@ -34,7 +34,7 @@ prompt = ChatPromptTemplate.from_template(
 llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
 
 # 「検索 => プロンプトの穴埋め => LLMで回答を生成」という連鎖（chain）を作成
-chain = (
+chain: Runnable = (
     {"context": retriever, "question": RunnablePassthrough()}
     | prompt
     | llm
